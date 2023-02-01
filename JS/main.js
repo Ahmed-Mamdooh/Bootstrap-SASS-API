e06d7eaf60765838dcd5d2073571497791eb085b
Ahmed Mamdooh <ahmedmamdoohsd@gmail.com>
2023-02-01 14:46:50 +0200

    first edition

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// function getUsersData() {
+//   fetch("https://dummyjson.com/users")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+// getUsersData();
+let example = document.getElementById("example");
+console.log(example);
+let xXx = example.cloneNode(true);
